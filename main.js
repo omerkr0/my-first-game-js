09340b80712e156e27ab6e82876fa17a1fb3e925
omerkr0 <omerkaramanx@gmail.com>
2023-12-15 03:43:36 +0300

    Added bombs

--- main.js
+++ main.js
@@ -18,9 +18,11 @@ var config = {
 
 var player;
 var stars;
+var bombs;
 var platforms;
 var cursors;
 var score = 0;
+var gameOver = false;
 var scoreText;
 
 var game = new Phaser.Game(config);
@@ -87,10 +89,14 @@ function create() {
     fill: "#000",
   });
 
+  bombs = this.physics.add.group();
+
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(bombs, platforms);
 
   this.physics.add.overlap(player, stars, collectStar, null, this);
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
 function update() {
@@ -118,4 +124,30 @@ function collectStar(player, star) {
 
   score += 10;
   scoreText.setText(`Score: ${score}`);
+
+  if (stars.countActive(true) === 0) {
+    stars.children.iterate(function (child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    var x =
+      player.x < 400
+        ? Phaser.Math.Between(400, 800)
+        : Phaser.Math.Between(0, 400);
+
+    var bomb = bombs.create(x, 16, "bomb");
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  }
+}
+
+function hitBomb(player, bomb) {
+  this.physics.pause();
+
+  player.setTint(0xff0000);
+
+  player.anims.play("turn");
+
+  gameOver = true;
 }
